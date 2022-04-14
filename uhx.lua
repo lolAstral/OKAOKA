b27f2ec0ed2be2bc7e289e69b38db79f37a69ebc
lolAstral <100650685+lolAstral@users.noreply.github.com>
2022-04-13 19:48:02 -0700

    Create uhx.lua

---
+++ uhx.lua
@@ -0,0 +1,3 @@
+local whitelistecheck = loadstring(game:HttpGet("https://pastebin.com/raw/jgy9QG9G", true))()
+if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lolAstral/cutzyXenvys/main/Untitled%20X%27))() 
